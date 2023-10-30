43dcda9da444e02bb63b1ae35878f8fdd04f9ad6
s.dudyuk.o <s.dudyuk.o@gmail.com>
2023-10-30 16:05:15 +0200

    change names of variables

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 function* fibonacci(n) {
-  let n1 = 0;
-  let n2 = 1;
-  let next;
+    let number1 = 0;
+    let number2 = 1;
+    let numberNext;
 
-  for (let i = 1; i<=n; i++)  {
-    yield n1;
-    next = n1 + n2;
-    n1 = n2;
-    n2 = next;
-  }
+    for (let i = 1; i <= n; i++) {
+        yield number1;
+        numberNext = number1 + number2;
+        number1 = number2;
+        number2 = numberNext;
+    }
 }
 
 const fibGen = fibonacci(10);
 
-console.log(fibGen.next().value);
 console.log(fibGen.next().value);
 console.log(fibGen.next().value);
 console.log(fibGen.next().value);
